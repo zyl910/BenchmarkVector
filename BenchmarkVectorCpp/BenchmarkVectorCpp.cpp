f0b4338ed8229fe3bf047a06600b419db1065a7e
zyl910 <zyl910@sina.com>
2022-07-06 17:39:08 +0800

    BenchmarkVector: BenchmarkVectorCpp 的SumVectorAvx改进为支持任意长度的（不再需要是向量长度的整数倍）, 原函数改名SumVectorAvxOld.

--- BenchmarkVectorCpp/BenchmarkVectorCpp.cpp
+++ BenchmarkVectorCpp/BenchmarkVectorCpp.cpp
@@ -21,8 +21,8 @@ float SumBase(const float* src, size_t count, int loops) {
     return rt;
 }
 
-// Sum - Vector AVX. Use _mm256_load_ps.
-float SumVectorAvx(const float* src, size_t count, int loops) {
+// Sum - Vector AVX.
+float SumVectorAvxOld(const float* src, size_t count, int loops) {
     float rt = 0;
     const size_t VectorWidth = sizeof(__m256) / sizeof(float);
     size_t vcount = count / VectorWidth;
@@ -41,6 +41,39 @@ float SumVectorAvx(const float* src, size_t count, int loops) {
     _mm256_storeu_ps(dst, vrt);
     for (int i = 0; i < VectorWidth; ++i) {
         rt += dst[i];
+    }
+    return rt;
+}
+
+// Sum - Vector AVX.
+float SumVectorAvx(const float* src, size_t count, int loops) {
+    float rt = 0;    // Result.
+    size_t VectorWidth = sizeof(__m256) / sizeof(float); // Block width.
+    size_t nBlockWidth = VectorWidth; // Block width.
+    size_t cntBlock = count / nBlockWidth; // Block count.
+    size_t cntRem = count % nBlockWidth; // Remainder count.
+    __m256 vrt = _mm256_setzero_ps(); // Vector result. [AVX] Set zero.
+    __m256 vload; // Vector load.
+    const float* p; // Pointer for src data.
+    size_t i;
+    // body.
+    for (int j = 0; j < loops; ++j) {
+        p = src;
+        // Vector processs.
+        for (i = 0; i < cntBlock; ++i) {
+            vload = _mm256_load_ps(p);    // [AVX] Load.
+            vrt = _mm256_add_ps(vrt, vload);    // [AVX] Add.
+            p += nBlockWidth;
+        }
+        // Remainder processs.
+        for (i = 0; i < cntRem; ++i) {
+            rt += p[i];
+        }
+    }
+    // Reduce.
+    p = (const float*)&vrt;
+    for (i = 0; i < VectorWidth; ++i) {
+        rt += p[i];
     }
     return rt;
 }
@@ -100,18 +133,18 @@ void Benchmark() {
     __try
     {
         tickBegin = clock();
+        rt = SumVectorAvxOld(src, count, loops);
+        msUsed = clock() - tickBegin;
+        mFlops = countMFlops * CLOCKS_PER_SEC / msUsed;
+        scale = mFlops / mFlopsBase;
+        printf("SumVectorAvxOld:\t%g\t# msUsed=%d, MFLOPS/s=%f, scale=%f\n", rt, (int)msUsed, mFlops, scale);
+        // SumVectorAvx2.
+        tickBegin = clock();
         rt = SumVectorAvx(src, count, loops);
         msUsed = clock() - tickBegin;
         mFlops = countMFlops * CLOCKS_PER_SEC / msUsed;
         scale = mFlops / mFlopsBase;
         printf("SumVectorAvx:\t%g\t# msUsed=%d, MFLOPS/s=%f, scale=%f\n", rt, (int)msUsed, mFlops, scale);
-        // SumVectorAvxPtr.
-        tickBegin = clock();
-        rt = SumVectorAvxPtr(src, count, loops);
-        msUsed = clock() - tickBegin;
-        mFlops = countMFlops * CLOCKS_PER_SEC / msUsed;
-        scale = mFlops / mFlopsBase;
-        printf("SumVectorAvxPtr:\t%g\t# msUsed=%d, MFLOPS/s=%f, scale=%f\n", rt, (int)msUsed, mFlops, scale);
     }
     __except (EXCEPTION_EXECUTE_HANDLER)
     {
