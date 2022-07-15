da2c56732addd208a4d6ae0d13a39e52bec374ba
zyl910 <zyl910@sina.com>
2022-07-15 10:58:06 +0800

    VectorClassDemo50: VectorClassDemo for .NET5 Console Application.

---
+++ VectorClassDemo/VectorClassDemo50/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using VectorClassDemo;
+
+namespace VectorClassDemo50 {
+    class Program {
+        static void Main(string[] args) {
+            string indent = "";
+            TextWriter tw = Console.Out;
+            tw.WriteLine("VectorClassDemo50");
+            tw.WriteLine();
+            VectorDemo.OutputEnvironment(tw, indent);
+            tw.WriteLine();
+            VectorDemo.Run(tw, indent);
+        }
+    }
+}
